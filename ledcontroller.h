3a9e33eb8986c7e049f842c6945f0c708407f422
Otto Ryynänen <otto.ryynanen@dnainternet.net>
2016-06-27 09:24:47 +0300

    Added missing Q_PROPERTY

--- ledcontroller.h
+++ ledcontroller.h
@@ -17,6 +17,7 @@ signals:
 private:
     Q_OBJECT
     Q_PROPERTY(int gpioPinNumber READ getGpioPinNumber WRITE setGpioPinNumber NOTIFY gpioPinNumberChanged)
+    Q_PROPERTY(bool ledOn READ getLedOn WRITE setLedOn NOTIFY ledOnChanged)
     int gpioPinNumber;
     bool ledOn;
 };
